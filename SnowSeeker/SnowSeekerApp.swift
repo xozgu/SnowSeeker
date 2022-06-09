8378e7bf428f13b666938412bd55a5833359d056
Ozgu Ozden <xozguozden@gmail.com>
2022-06-09 21:25:06 +0900

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Ozgu Ozden on 2022/06/09.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
